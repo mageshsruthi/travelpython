3213ae932dbf89edf70fcc30cc9d2ce19f746ef2
mageshsruthi <psreekutty06@gmail.com>
2024-02-01 11:27:39 +0530

    added

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def demo(request):
+    name="india"
+    return render(request,"index.html",{'obj':name})
